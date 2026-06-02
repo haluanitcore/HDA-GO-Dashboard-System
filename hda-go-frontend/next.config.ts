d87aa1d057d4ecf3a39376ba9cb8d1d1bf6f6dbb
Irfan Pandu Aji <137133320+Irfanpanduaji@users.noreply.github.com>
2026-06-02 16:36:54 +0700

    build: force single thread sequential compilation for low memory vps

--- hda-go-frontend/next.config.ts
+++ hda-go-frontend/next.config.ts
@@ -4,9 +4,10 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  }
 };
 
 export default nextConfig;
